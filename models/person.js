5a4a86ca4b7da90a42def67e3b069c58d487b939
Daniel Kapica <kapica6105@gmail.com>
2018-10-29 10:58:14 +0000

    Completed 2 tests

--- models/person.js
+++ models/person.js
@@ -1,1 +1,15 @@
 // Your Person class goes here!
+
+class Person {
+    constructor(firstName, lastName, DoB) {
+        this.firstName = capitalise(firstName)
+        this.lastName = lastName
+        this.DoB = DoB
+    }
+}
+
+function capitalise(firstName){
+    return firstName.charAt(0).toUpperCase()+firstName.slice(1)
+}
+
+module.exports = Person
--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,17 @@
 const chai = require('chai')
 const expect = chai.expect
-
+const Person = require('../models/person.js')
 // Your Person test goes here!
+
+describe('Person', ()=> {
+    it('Should initialise properly', ()=>{
+        var person = new Person('Joe','Bloggs','1 Jan 1990')
+        expect(person.firstName).to.equal('Joe')
+        expect(person.lastName).to.equal('Bloggs')
+        expect(person.DoB).to.equal('1 Jan 1990')
+    })
+    it('Should capitalise first name', ()=>{
+        var person = new Person('amy','Nether','25 Jun 1982')
+        expect(person.firstName).to.equal('Amy')
+    })
+})
