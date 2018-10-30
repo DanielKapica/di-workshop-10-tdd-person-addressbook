e2421aebcf3d2938c6b48f87120376d964dea673
Daniel Kapica <kapica6105@gmail.com>
2018-10-30 14:50:15 +0000

    Completed some tests

--- models/person.js
+++ models/person.js
@@ -2,14 +2,33 @@
 
 class Person {
     constructor(firstName, lastName, DoB) {
-        this.firstName = capitalise(firstName)
+        this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1)
         this.lastName = lastName
         this.DoB = DoB
+        this.emails = []
+        this.phoneNumbers = []
+    }
+    fullName() {
+        return this.firstName + ' ' + this.lastName
+    }
+
+    addEmail(emailAddress) {
+        this.emails.push(emailAddress)
+    }
+
+    addPhoneNumber(phoneNumber){
+        this.phoneNumbers.push(phoneNumber)
+    }
+    returnFormattedDetails(){
+        var lines = [
+            this.fullName(),
+            "------------",
+            "DOB: " + this.DoB,
+            "/n",
+            "Email Addresses",
+            "-" + this.emails
+        ]
+        return lines
     }
 }
-
-function capitalise(firstName){
-    return firstName.charAt(0).toUpperCase()+firstName.slice(1)
-}
-
 module.exports = Person
--- test/person.test.js
+++ test/person.test.js
@@ -3,15 +3,35 @@ const expect = chai.expect
 const Person = require('../models/person.js')
 // Your Person test goes here!
 
-describe('Person', ()=> {
-    it('Should initialise properly', ()=>{
-        var person = new Person('Joe','Bloggs','1 Jan 1990')
+describe('Person', () => {
+    it('Should initialise properly', () => {
+        var person = new Person('Joe', 'Bloggs', '1 Jan 1990')
         expect(person.firstName).to.equal('Joe')
         expect(person.lastName).to.equal('Bloggs')
         expect(person.DoB).to.equal('1 Jan 1990')
+        expect(person.emails).to.deep.equal([])
     })
-    it('Should capitalise first name', ()=>{
-        var person = new Person('amy','Nether','25 Jun 1982')
+    it('Should capitalise first name', () => {
+        var person = new Person('amy', 'Nether', '25 Jun 1982')
         expect(person.firstName).to.equal('Amy')
+    })
+    it('Should return full name', () => {
+        var person = new Person('Joe', 'Bloggs', '1 Mar 1990')
+        expect(person.fullName()).to.equal('Joe Bloggs')
+    })
+    it('Should accept new email and put them into an array', () => {
+        var person = new Person('Danielle', 'Goodie', '21 Dec 2000')
+        person.addEmail('dangoodie@email.com')
+        expect(person.emails).to.deep.equal(['dangoodie@email.com'])
+    })
+    it('Should accept new phone numbers and put them into an array', () => {
+        var person = new Person('George', 'Bush', '7 Feb 1989')
+        person.addPhoneNumber('07986123645')
+        expect(person.phoneNumbers).to.deep.equal(['07986123645'])
+    })
+    it('Should return details',()=>{
+        var person = new Person('George', 'Bush', '7 Feb 1989')
+        person.addEmail('georgebush@gmail.com')
+        expect(person.returnFormattedDetails()).to.deep.equal(['George Bush',"------------","DOB: 7 Feb 1989","/n","Email Addresses", "-georgebush@gmail.com"])
     })
 })
